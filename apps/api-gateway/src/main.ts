456edd6fa14746fd5c9bbb0e93fb310ca1decdac
Antarin Ghosal <antaringhosal@gmail.com>
2025-10-28 02:54:09 +0530

    add rate limiting and health check endpoint to api-gateway; update auth-service for CORS support

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -4,18 +4,51 @@
  */
 
 import express from 'express';
-import * as path from 'path';
 import cors from 'cors';
 import proxy from 'express-http-proxy';
 import morgan from 'morgan';
+import { rateLimit, ipKeyGenerator } from 'express-rate-limit';
+import swaggerUi from 'swagger-ui-express';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  allowedHeaders: ['Authorization', 'Content-Type'],
+  credentials: true,
+}));
 
-app.get('/api', (req, res) => {
+app.use(morgan('dev'));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(cookieParser());
+app.set('trust proxy', 1);
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: {error: 'Too many requests from this IP, please try again later.'},
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req: any) => {
+ 		// Use API key (or some other identifier) for authenticated users
+    if (req.query.apiKey) return req.query.apiKey
+
+ 		// fallback to IP for unauthenticated users
+		// return req.ip // vulnerable
+		return ipKeyGenerator(req.ip) // better
+	}
+});
+app.use(limiter);
+
+app.get('/gateway-health', (req, res) => {
   res.send({ message: 'Welcome to api-gateway!' });
 });
+
+app.get('/', proxy("http://localhost:6001"));
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
