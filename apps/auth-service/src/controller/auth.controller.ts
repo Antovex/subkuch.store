ce50ba872dbb53946313c55c13a39cbf74402943
Antarin Ghosal <antaringhosal@gmail.com>
2025-11-01 01:42:32 +0530

    add email sending functionality with EJS templates
    - Added nodemailer for sending emails.
    - Created a utility function to send emails using EJS templates.
    - Introduced a new email template for user activation.
    - Updated package.json to include necessary type definitions for nodemailer and ejs.

--- apps/auth-service/src/controller/auth.controller.ts
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -1,19 +1,41 @@
 import { Request, Response, NextFunction } from "express";
-import { validateRegistrationData, checkOtpRestrictions } from "../utils/auth.helper";
+import {
+    validateRegistrationData,
+    checkOtpRestrictions,
+    trackOtpRequest,
+    sendOtp,
+} from "../utils/auth.helper";
 import prisma from "../../../../packages/libs/prisma";
 import { ValidationError } from "../../../../packages/error-handler";
 
-export const userRegistration = async (req: Request, res: Response, next: NextFunction) => {
-    validateRegistrationData(req.body, "user");
-    const {name, email} = req.body;
+export const userRegistration = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        validateRegistrationData(req.body, "user");
+        const { name, email } = req.body;
 
-    const exsistingUser = await prisma.user.findUnique({
-        where: { email }
-    });
+        const exsistingUser = await prisma.user.findUnique({
+            where: { email },
+        });
 
-    if(exsistingUser) {
-        return next(new ValidationError("User with this email already exists"));
+        if (exsistingUser) {
+            return next(
+                new ValidationError("User with this email already exists")
+            );
+        }
+
+        await checkOtpRestrictions(email, next);
+        await trackOtpRequest(email, next);
+        await sendOtp(name, email, "user-activation-mail");
+
+        res.status(200).json({
+            message:
+                "OTP sent to your email. Please verify to complete registration.",
+        });
+    } catch (error) {
+        return next(error);
     }
-
-    await checkOtpRestrictions(email, next);
-}
+};
--- apps/auth-service/src/utils/auth.helper.ts
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -2,6 +2,7 @@ import crypto from "crypto";
 import { ValidationError } from "../../../../packages/error-handler";
 import { NextFunction } from "express";
 import redis from "../../../../packages/libs/redis";
+import { sendEmail } from "./sendMail";
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -25,13 +26,61 @@ export const validateRegistrationData = (
     }
 };
 
-export const checkOtpRestrictions = async (email: string, next: NextFunction) => {
+export const checkOtpRestrictions = async (
+    email: string,
+    next: NextFunction
+) => {
+    if (await redis.get(`otp_lock:${email}`)) {
+        return next(
+            new ValidationError(
+                "Account locked due to multiple failed OTP attempts. Please try again after 30 minutes."
+            )
+        );
+    }
 
-}
+    if (await redis.get(`otp_spam_lock:${email}`)) {
+        return next(
+            new ValidationError(
+                "Too many OTP requests. Please try again after 1 hour."
+            )
+        );
+    }
 
-export const sendOtp = async (name: string, email: string, template: string) => {
+    if (await redis.get(`otp_cooldown:${email}`)) {
+        return next(
+            new ValidationError(
+                "Please wait 1 mintue before requesting a new OTP."
+            )
+        );
+    }
+};
+
+export const sendOtp = async (
+    name: string,
+    email: string,
+    template: string
+) => {
     const otp = crypto.randomInt(1000, 9999).toString();
-    // await sendEmail();
-    await redis.set(`otp:${email}`, otp, 'EX', 300); // OTP valid for 5 minutes
-    await redis.set(`otp_cooldown:${email}`, "true", 'EX', 60); // Cooldown of 1 minute
+    await sendEmail(email, "Verify your Email...", template, { name, otp });
+    await redis.set(`otp:${email}`, otp, "EX", 300); // OTP valid for 5 minutes
+    await redis.set(`otp_cooldown:${email}`, "true", "EX", 60); // Cooldown of 1 minute
+};
+
+export const trackOtpRequest = async (
+    email: string,
+    next: NextFunction
+) => {
+    const otpRequestKey = `otp_request_count:${email}`;
+    let requestCount = parseInt(await redis.get(otpRequestKey) || "0");
+
+    if(requestCount >= 2) {
+        await redis.set(`otp_spam_lock:${email}`, "locked", "EX", 3600); // 1 hour lock
+        return next(
+            new ValidationError(
+                "Too many OTP requests. Please try again after 1 hour."
+            )
+        );
+    }
+
+    await redis.set(otpRequestKey, requestCount + 1, "EX", 3600); // tracking otp of particular email for 1 hour
 }
