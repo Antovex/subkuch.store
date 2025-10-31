16e15ebbc6947ea9cf7b4d41d3b4eecb50307e7d
Antarin Ghosal <antaringhosal@gmail.com>
2025-10-31 20:48:33 +0530

    Add Redis configuration and integrate OTP functionality in auth service

--- apps/auth-service/src/utils/auth.helper.ts
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -1,6 +1,7 @@
 import crypto from "crypto";
 import { ValidationError } from "../../../../packages/error-handler";
 import { NextFunction } from "express";
+import redis from "../../../../packages/libs/redis";
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -24,4 +25,13 @@ export const validateRegistrationData = (
     }
 };
 
-export const checkOtpRestrictions = async (email: string, next: NextFunction) => {}
+export const checkOtpRestrictions = async (email: string, next: NextFunction) => {
+
+}
+
+export const sendOtp = async (name: string, email: string, template: string) => {
+    const otp = crypto.randomInt(1000, 9999).toString();
+    // await sendEmail();
+    await redis.set(`otp:${email}`, otp, 'EX', 300); // OTP valid for 5 minutes
+    await redis.set(`otp_cooldown:${email}`, "true", 'EX', 60); // Cooldown of 1 minute
+}
