245229920cf600ba73d177ebbe923d3d4b502d1b
Antarin Ghosal <antaringhosal@gmail.com>
2025-11-03 02:32:30 +0530

    Refactor code structure for improved readability and maintainability

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,8 +1,10 @@
-import express from "express";
+﻿import express from "express";
 import cors from "cors";
 import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
 import cookieParser from "cookie-parser";
 import router from "./routes/auth.router";
+import swaggerUi from 'swagger-ui-express';
+const swaggerUiDocument = require('./swagger/swagger-document.json');
 
 const app = express();
 
@@ -21,6 +23,10 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello API" });
 });
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerUiDocument));
+app.get("/docs-josn", (req, res) => {
+  res.json(swaggerUiDocument);
+})
 // Routes
 app.use('/api', router);
 
@@ -30,6 +36,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`);
+  console.log(`API Docs at http://localhost:${port}/docs`); ///docs-josn
 });
 
 server.on("error", (err) => {
